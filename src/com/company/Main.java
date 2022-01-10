cb9a438653ef9ce75ff52d1bae7ab1988ac32d8e
Paulina <paulina_szreder@wp.pl>
2022-01-10 12:20:49 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i <=50; i+=1){
+            System.out.println("Progrogram działa. Foobar");
+            System.out.println(i);
+            pause(1000);
+            System.out.flush();
+        }
+    }
+
+
+    public static void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+}
